475c51470aba37e5e2b63c1c9ff318ab4b96bd45
prachi05-pr <prachipaunikar05@gmail.com>
2026-08-16 12:22:53 +0530

    Time: 42 ms (44.9%), Space: 53.1 MB (11.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,8 +3,6 @@ public:
     int dp2(vector<int>& coins,int amount,vector<vector<int>> &dp){
     int n=coins.size();
 
-
-        // Amount 0 requires 0 coins
         for(int i = 0; i <= n; i++) {
             dp[i][0] = 0;
         }
@@ -20,7 +18,7 @@ public:
             dp[i][j]= min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
             }
 
-            else{
+            else{   //invalid case 
             dp[i][j]= dp[i-1][j];
             }
 
@@ -34,7 +32,7 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        //unbounded knapsack
+        //unbounded knapsack sarkha ahe
         vector<vector<int>> dp(n+1,vector<int>(amount +1,0));
        return dp2(coins,amount,dp);
     }

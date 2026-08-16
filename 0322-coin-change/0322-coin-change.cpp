6f76c1e819fb341f20f97ccc0fac37afe83404c4
prachi05-pr <prachipaunikar05@gmail.com>
2026-08-16 12:19:23 +0530

    Time: 53 ms (20.88%), Space: 53 MB (11.33%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int dp2(vector<int>& coins,int amount,vector<vector<int>> &dp){
+    int n=coins.size();
+
+
+        // Amount 0 requires 0 coins
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Impossible initially
+        for(int j = 1; j <= amount; j++) {
+            dp[0][j] = amount + 1;
+        }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<amount+1;j++){
+            if(coins[i-1]<=j){  //valid
+            dp[i][j]= min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+            }
+
+            else{
+            dp[i][j]= dp[i-1][j];
+            }
+
+        }
+    }
+
+     if(dp[n][amount] > amount)
+            return -1;
+
+    return dp[n][amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        //unbounded knapsack
+        vector<vector<int>> dp(n+1,vector<int>(amount +1,0));
+       return dp2(coins,amount,dp);
+    }
+};

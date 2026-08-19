9986daabb445e33f2bfe3d19501165906c2da2a4
prachi05-pr <prachipaunikar05@gmail.com>
2026-08-19 17:10:14 +0530

    Time: 48 ms (30.07%), Space: 53 MB (13.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -9,13 +9,14 @@ public:
 
         // Impossible initially
         for(int j = 1; j <= amount; j++) {
-            dp[0][j] = amount + 1;
+            dp[0][j] = amount +1;
         }
 
     for(int i=1;i<n+1;i++){
         for(int j=1;j<amount+1;j++){
             if(coins[i-1]<=j){  //valid
-            dp[i][j]= min( 1+ dp[i][j-coins[i-1]], dp[i-1][j]);
+            dp[i][j] = min(1+ dp[i][j - coins[i-1]] , dp[i-1][j]);
+            //dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
             }
 
             else{   //invalid case 
